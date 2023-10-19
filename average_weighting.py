ffe4657a6ccfad8292e254e9090892a8245a1a5b
Soham <sohamsarode2312@gmail.com>
2023-10-19 10:45:04 +0530

    Second one added

--- average_weighting.py
+++ average_weighting.py
@@ -10,26 +10,36 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 movies_cleaned_df = pd.read_pickle('movies_cleaned_df.pkl')
+tfv_matrix = pkl.load(open('tfv_matrix.pkl','rb'))
 
 from sklearn.metrics.pairwise import sigmoid_kernel
 
 # Compute the sigmoid kernel
 sig = sigmoid_kernel(tfv_matrix, tfv_matrix)
-# Reverse mapping of indices and movie titles
-indices = pd.Series(movies_cleaned_df.index, index=movies_cleaned_df['original_title']).drop_duplicates()
-indices
-new_df = pd.read_pickle('movies.pkl') 
-similarity = pkl.load(open('similarity.pkl','rb'))
+
+indices=pkl.load(open('indices.pkl','rb'))
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    movie = request.get_json()['movie']
-    movie_index = new_df[new_df['title'] == movie].index[0]
-    distances = similarity[movie_index]
-    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
-    recommended_movies = [new_df.iloc[i[0]].title for i in movie_list]
+    title = request.get_json()['movie']
+    idx = indices[title]
+
+    # Get the pairwsie similarity scores 
+    sig_scores = list(enumerate(sig[idx]))
+
+    # Sort the movies 
+    sig_scores = sorted(sig_scores, key=lambda x: x[1], reverse=True)
+
+    # Scores of the 10 most similar movies
+    sig_scores = sig_scores[1:11]
+
+    # Movie indices
+    movie_indices = [i[0] for i in sig_scores]
+
+    # Top 10 most similar movies
+    recommended_movies= movies_cleaned_df['original_title'].iloc[movie_indices]
     
-    return jsonify({'movies': recommended_movies})
+    return jsonify({'movies': recommended_movies.tolist()})
 
 if __name__ == "__main__":
     app.run()
